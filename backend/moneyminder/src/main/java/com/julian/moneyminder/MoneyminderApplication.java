0e9efa07dd37cbb81ed5badf88da78681f0fad11
julianc06 <william.castaneda03@uptc.edu.co>
2024-08-15 23:17:22 -0500

    Add Spring project in backend

---
+++ backend/moneyminder/src/main/java/com/julian/moneyminder/MoneyminderApplication.java
@@ -0,0 +1,13 @@
+package com.julian.moneyminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyminderApplication.class, args);
+    }
+
+}
